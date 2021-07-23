4256e8a40d2d3e32aa35c302761de7043a0bd204
aduncanson <36767650+aduncanson@users.noreply.github.com>
2021-07-23 15:52:36 +0100

    Update some_basic_tests.py

--- some_basic_tests.py
+++ some_basic_tests.py
@@ -6,12 +6,14 @@ from some_basic_code import my_basic_function
 # Unittest boilerplate
 class TestStringMethods(unittest.TestCase):
     # We want one test to fail to demonstrate CI build failure
+    '''
     def test_my_basic_function_fails(self):
         self.assertEqual(my_basic_function(), "") # AssertionError
+    '''
         
-        # And one test to pass
-        def test_my_basic_function_passes(self):
-            self.assertEqual(my_basic_function(), "Hi from my basic function!") # Pass!
+    # And one test to pass
+    def test_my_basic_function_passes(self):
+        self.assertEqual(my_basic_function(), "Hi from my basic function!") # Pass!
 
 if __name__ == '__main__':
     unittest.main()
