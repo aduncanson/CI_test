1866cfdb08550e97b699c364321a9abe07c2dd8e
aduncanson <36767650+aduncanson@users.noreply.github.com>
2021-07-27 22:40:22 +0100

    Create lambda_function.py

---
+++ lambda_function.py
@@ -0,0 +1,24 @@
+import sys
+import json
+
+
+def my_func(string):
+    print('Hello from Lambda!')
+    print(sys.version)
+    return string.upper()
+
+
+def lambda_handler(event, context):
+    print(my_func("foo"))
+
+    return {
+        # Required values when using Lambda Proxy Intergration:
+
+        # statusCode
+        # headers
+        # body
+
+        'statusCode': 200,
+        'headers': {},
+        'body': json.dumps(event)
+    }
